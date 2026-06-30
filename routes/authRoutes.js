7f45232d6bc25ee88533e6a21e7dcbb8b52617b3
razanbhandary-lgtm <razanbhandary@gmail.com>
2026-06-30 22:53:31 +0545

    Create routes/authRoutes.js

---
+++ routes/authRoutes.js
@@ -0,0 +1,21 @@
+const express=require("express");
+
+const router=express.Router();
+
+const auth=require("../controllers/authController");
+
+const{
+verifyToken
+}=require("../middleware/auth");
+
+router.post("/register",auth.register);
+
+router.post("/login",auth.login);
+
+router.get("/me",verifyToken,(req,res)=>{
+
+res.json(req.user);
+
+});
+
+module.exports=router;
